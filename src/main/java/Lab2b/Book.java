de43190ae4d22182a47a8b05523188c78c0b12d4
wanghelin215 <1148291406@qq.com>
2024-02-24 11:17:22 +0800

    lab2 completed

---
+++ src/main/java/Lab2b/Book.java
@@ -0,0 +1,27 @@
+package Lab2b;
+public class Book {
+    private String chapters[];
+    private static final int DEFAULT_CHAPTERS = 10;
+
+    public Book() {
+        chapters = new String[DEFAULT_CHAPTERS];
+        for (int i = 0; i < chapters.length; i++)
+            chapters[i] = "n/a";
+    }
+    public Book(String argument[]) {
+        /* construct the object with an array of chapter names */
+        /* PLEASE ADD YOUR CODE HERE */
+        int chapter_length = argument.length;
+        chapters = new String[chapter_length];
+        for (int i = 0; i < chapter_length; i++)
+            chapters[i] = argument[i];
+    }
+    public String getChapter(int i) {
+        /* return the chapter by the given index */
+        /* PLEASE ADD YOUR CODE HERE */
+        return chapters[i];
+    }
+    public String[] getChapters() {
+        return chapters;
+    }
+}
